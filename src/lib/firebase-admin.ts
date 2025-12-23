5c1b464d04b9ad5639fc9ef53ccdd8671e3582cb
Aymen A4 <afia.med.aymen@gmail.com>
2025-12-23 14:35:08 +0000

    Console Error Server
    2 UNKNOWN: Getting metadata from plugin failed wi

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,11 +1,7 @@
 import * as admin from 'firebase-admin';
-import { firebaseConfig } from '@/firebase/config';
 
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
-    projectId: firebaseConfig.projectId,
-  });
+  admin.initializeApp();
 }
 
 export const firestoreAdmin = admin.firestore();
