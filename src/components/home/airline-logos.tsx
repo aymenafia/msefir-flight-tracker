024a2bc2b9d17d2fd929d915464bf3198fadb014
Aymen A4 <afia.med.aymen@gmail.com>
2025-12-23 16:10:22 +0000

    dont create logo remove all files tsx in airlines folder. diplay exact i

--- src/components/home/airline-logos.tsx
+++ src/components/home/airline-logos.tsx
@@ -1,11 +1,15 @@
 'use client';
 import { useTranslation } from '@/hooks/use-translation';
 import { Card, CardContent } from '@/components/ui/card';
-import { RoyalAirMarocLogo } from '../icons/airlines/RoyalAirMarocLogo';
-import { AirAlgerieLogo } from '../icons/airlines/AirAlgerieLogo';
-import { TunisairLogo } from '../icons/airlines/TunisairLogo';
-import { NouvelairLogo } from '../icons/airlines/NouvelairLogo';
-import { AirArabiaMarocLogo } from '../icons/airlines/AirArabiaMarocLogo';
+import Image from 'next/image';
+
+const airlines = [
+  { name: 'Royal Air Maroc', logo: '/images/airlines/royal-air-maroc.png' },
+  { name: 'Air Algérie', logo: '/images/airlines/air-algerie.png' },
+  { name: 'Tunisair', logo: '/images/airlines/tunisair.png' },
+  { name: 'Nouvelair', logo: '/images/airlines/nouvelair.png' },
+  { name: 'Air Arabia Maroc', logo: '/images/airlines/air-arabia-maroc.png' },
+];
 
 export function AirlineLogos() {
   const { t } = useTranslation();
@@ -17,11 +21,18 @@ export function AirlineLogos() {
       <Card>
         <CardContent className="p-8">
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-y-8 gap-x-4 items-center justify-center">
-            <RoyalAirMarocLogo className="h-10 w-auto mx-auto" />
-            <AirAlgerieLogo className="h-12 w-auto mx-auto" />
-            <TunisairLogo className="h-10 w-auto mx-auto" />
-            <NouvelairLogo className="h-8 w-auto mx-auto" />
-            <AirArabiaMarocLogo className="h-12 w-auto mx-auto" />
+            {airlines.map((airline) => (
+              <div key={airline.name} className="flex justify-center">
+                <Image
+                  src={airline.logo}
+                  alt={`${airline.name} logo`}
+                  width={160}
+                  height={40}
+                  className="h-10 w-auto object-contain"
+                  unoptimized
+                />
+              </div>
+            ))}
           </div>
         </CardContent>
       </Card>
