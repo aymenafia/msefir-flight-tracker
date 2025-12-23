d145a766e24a2b1c4727818f2f3b877413fcb250
Aymen A4 <afia.med.aymen@gmail.com>
2025-12-23 14:22:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4944795684-cc004",
+  "appId": "1:744938717788:web:f5e784bbab563358cbdc99",
+  "apiKey": "AIzaSyBntDZqvwDwH8vggar0w0_9cTk0w9b6H_M",
+  "authDomain": "studio-4944795684-cc004.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "744938717788"
+};
