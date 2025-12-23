d239e94b1c28b69b908f9c3e8a229f9017926667
Aymen A4 <afia.med.aymen@gmail.com>
2025-12-23 14:29:30 +0000

    Try fixing this error: `Console Error: 2 UNKNOWN: Getting metadata from

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,16 +1,9 @@
 import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
-  try {
-    // In a deployed environment, `applicationDefault` should find the credentials.
-    // For local development, you need to set up the GOOGLE_APPLICATION_CREDENTIALS
-    // environment variable to point to your service account key file.
-    admin.initializeApp({
-      credential: admin.credential.applicationDefault(),
-    });
-  } catch (e) {
-    console.error('Firebase Admin initialization error', e);
-  }
+  admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+  });
 }
 
 export const firestoreAdmin = admin.firestore();
