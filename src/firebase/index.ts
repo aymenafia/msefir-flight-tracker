2040a90c7dc977eee75cb77c175704c309b9c74d
Aymen A4 <afia.med.aymen@gmail.com>
2025-12-24 19:22:42 +0000

    are you did a fix ?

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -8,11 +8,9 @@ import { getFirestore } from 'firebase/firestore'
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
   if (getApps().length > 0) {
-    // If an app is already initialized, return the existing instance.
     return getSdks(getApp());
   }
   
-  // If no app is initialized, create one with the provided config.
   const firebaseApp = initializeApp(firebaseConfig);
   return getSdks(firebaseApp);
 }
